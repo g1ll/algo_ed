8ed34f7c555f2348d181c11443ae8c7bd7e8b927
Gill <gillvelleda@gmail.com>
2020-01-09 12:15:53 -0300

    Fix Exercício 1.2-2

--- cormen2012/part_I/ch1/exer_1_2_2.m
+++ cormen2012/part_I/ch1/exer_1_2_2.m
@@ -3,12 +3,14 @@
 % Suponha que estamos comparando implementações de ordenação por inserção e ordenação por
 % intercalação na mesma máquina. Para entradas de tamanho n, a ordenação por inserção é 
 % executada em 8n² passos, enquanto a ordenação por intercalação é executada em 64nlgn passos.
-% Para quais valores de n a ordenação por inserção supera a ordenação por intercalação?
+% Para quais valores de n a ordenação por inserção supera em velocidade a ordenação por intercalação?
 %
 % 
-% 8n² < 64nlgn => n < 8nlgn => 2<n<43
+% 8n² < 64nlgn => n < 8nlgn => 2< n <43
 % 8n² > 64nlgn => n > 8nlgn => n >= 44
-
+%
+% Para valores de 2 < n < 43 a ordenação por inserção é mais rápida do que a ordenação por
+%intercalação (merg sort)
 
 format shortG
 more off
