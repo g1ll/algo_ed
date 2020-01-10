120e6912f3de88835ce194a3333ab32b96368ef4
Gill <gillvelleda@gmail.com>
2020-01-10 18:57:41 -0300

    Ordem decrescente

---
+++ cormen2012/part_I/ch2/insertSortRev.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void insertionSortRev(int size,int v[size]);
+
+int main(int argc,char **argv){
+    
+    if(argc<=1){
+        printf("\nErro: argc<=1\n");
+        return 1;
+    }else{
+        int n=argc-1,v[n];
+        for(int i=0;i<n;i++)v[i]=atoi(argv[i+1]);
+        insertionSortRev(n,v);    
+        printf("\n\n\n");
+        getchar();
+        return 0;
+    }
+
+}
+
+void insertionSortRev(int size, int v[size]){
+    int key;
+    printf("\nV (in): ");
+    for(int i=0;i<size;i++)printf(" %d",v[i]);
+    for(int j=1,i=0;j<size;j++,i=j-1){
+        key = v[j];
+        //insere v[j] na sequência ordenada 
+        while(i>=0&&v[i]<key){
+            v[i+1]=v[i];
+            i--;
+        }
+        v[i+1]=key;
+    }
+    printf("\nV(out): ");
+    for(int i=0;i<size;i++)printf(" %d",v[i]);
+}
